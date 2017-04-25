f61a707e3c65ea2dc55b2bc2cfebcad0ef0c08d6
Patrick Cartlidge <patrickcartlidge@gmail.com>
2017-04-25 15:47:58 +0100

    the fix wont work on prod and i dont know why (yet)

--- app/controllers/alchemy/pages_controller.rb
+++ app/controllers/alchemy/pages_controller.rb
@@ -33,7 +33,7 @@ module Alchemy
     rescue_from ActionController::UnknownFormat, with: :page_not_found!
 
     def redirect_to_root_if_root_page_without_appending_page_name
-      if Language.current.pages.contentpages.find_by(urlname: params[:urlname], language_code: Language.current.code).try(:language_root)
+      if Language.current.pages.contentpages.find_by(urlname: params[:urlname], language_code: Language.current.code).try(:language_root) && params[:urlname] == 'home'
         redirect_to "/" and return
       end
     end
