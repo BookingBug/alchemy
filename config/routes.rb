5c1374e7d29d93ae93479018ba6a8e5f65accbf9
Patrick Cartlidge <patrickcartlidge@gmail.com>
2017-04-26 10:54:36 +0100

    trying something else to do redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ require 'alchemy/routing_constraints'
 Alchemy::Engine.routes.draw do
   root to: 'pages#index'
 
+  get '/home' => redirect("/")
+
   get '/sitemap.xml' => 'pages#sitemap', format: 'xml'
 
   scope Alchemy.admin_path, {constraints: Alchemy.admin_constraints} do
